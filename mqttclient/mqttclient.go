674d6600a280b1661f52ba8337547936798461e8
Walter Vargas <waltervargas@linux.com>
2022-08-17 23:46:05 +0200

    Address comments

--- mqttclient/mqttclient.go
+++ mqttclient/mqttclient.go
@@ -10,18 +10,18 @@ type Client struct {
 	mqttClient mqtt.Client
 }
 
-func New(server, id string) Client {
+func New(server, id string) *Client {
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker(server).SetClientID(id)
 
-	return Client{
+	return &Client{
 		server:     server,
 		id:         id,
 		mqttClient: mqtt.NewClient(opts),
 	}
 }
 
-func (c Client) connect() error {
+func (c *Client) connect() error {
 	if token := c.mqttClient.Connect(); token.Wait() && token.Error() != nil {
 		return token.Error()
 	}
@@ -29,17 +29,17 @@ func (c Client) connect() error {
 	return nil
 }
 
-func Connect(server, id string) (Client, error) {
+func Connect(server, id string) (*Client, error) {
 	c := New(server, id)
 	err := c.connect()
 	if err != nil {
-		return Client{}, err
+		return &Client{}, err
 	}
 
 	return c, nil
 }
 
-func (c Client) PublishMessage(path, message string) error {
+func (c *Client) PublishMessage(path, message string) error {
 	token := c.mqttClient.Publish(path, 1, false, message)
 	token.Wait()
 	if token.Error() != nil {
